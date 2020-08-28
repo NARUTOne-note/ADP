2caaace04529c5a925b58d77dafd170999869910
NARUTOne <wznaruto326@163.com>
2020-08-28 11:57:30 +0800

    stack getmin

---
+++ algorithm/stack/getMin-stack.js
@@ -0,0 +1,42 @@
+/**
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+示例:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+ */
+
+class MinStack {
+  constructor () {
+    this.length = 0;
+    this.content = [];
+    this.mins = [];
+  }
+  push (val) {
+    const curMin = this.mins[this.length - 1] !== undefined ? this.mins[this.length - 1] : Infinity;
+    this.content[this.length++] = val;
+    this.mins[this.length - 1] = Math.min(curMin, val);
+  }
+  pop () {
+    const num = this.content[--this.length]
+    this.content.pop();
+    return num;
+  }
+  top () {
+    return this.content[this.length - 1]
+  }
+  getMin () {
+    return this.mins[this.length - 1];
+  }
+}

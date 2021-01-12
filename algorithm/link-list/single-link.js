14c421a8bfef7e7ade5a918f8f6a22e5cd747c00
NARUTOne <wznaruto326@163.com>
2021-01-12 15:59:12 +0800

    link 链表 LeetCode

--- algorithm/link-list/single-link.js
+++ algorithm/link-list/single-link.js
@@ -4,8 +4,8 @@
  * ? 插入或删除：在某一节点后插入或删除一个节点（后继节点）的时间复杂度为 O(1)
  */
 
-function Node (element) {
-  this.element = element;
+function Node (val) {
+  this.val = val;
   this.next = null;
 }
 
@@ -16,12 +16,23 @@ function SingleLink () {
   this.length = 0
 }
 
-SingleLink.prototype.createNode = function (element) {
-  return new Node(element);
+SingleLink.prototype.createNode = function (val) {
+  return new Node(val);
 }
 
-SingleLink.prototype.append = function(element) {
-  let node = new Node(element);
+//遍历节点
+SingleLink.prototype.disPlay = function() {
+  let res = new Array()
+  let current = this.head
+  while (current !== null) {
+    res.push(current.val)
+    current = current.next
+  }
+  return res
+}
+
+SingleLink.prototype.append = function(val) {
+  let node = new Node(val);
   let p = this.heade;
   if (!this.head) {
     this.head = node;
@@ -34,18 +45,39 @@ SingleLink.prototype.append = function(element) {
   this.length += 1;
 }
 
-SingleLink.prototype.getNode = function (element) {
+SingleLink.prototype.getNode = function (val) {
   let p = this.header;
   if (!p) return null;
   while (p) {
-    if (p.element === element) return p;
+    if (p.val === val) return p;
     p = p.next;
   }
   return null;
 }
 
-SingleLink.prototype.insertNode = function (pos, element) {
-  const node = this.createNode(element);
+// 获取值为nodeVal的前一个节点,找不到为-1,参数是val
+// 适用于链表中无重复节点
+SingleLink.prototype.findNodePreByVal = function (nodeVal) {
+  let current = this.head;
+  while (current.next !== null && current.next.val !== nodeVal)
+    current = current.next
+  return current !== null ? current : -1
+}
+
+// 根据index查找当前节点, 参数为index
+SingleLink.prototype.findByIndex = function (index) {
+  let current = this.head,
+    pos = 1
+  while (current.next !== null && pos !== index) {
+    current = current.next
+    pos++
+  }
+
+  return (current && pos === index) ? current : -1
+}
+
+SingleLink.prototype.insertNode = function (pos, val) {
+  const node = this.createNode(val);
 
   if (pos < 0 || pos > this.length) return;
 
@@ -95,9 +127,9 @@ SingleLink.prototype.deleteNode = function (pos) {
 // ! 循环单链表： 首尾相连的环   next1 - next2 - ... - next1- next2 - ...
 
 // 插入 position 的后继节点: 遍历到尾节点，然后在尾节点后插入节点
-function insert (position, element) {
+function insert (position, val) {
   // 创建插入节点
-  let node = new createNode(element)
+  let node = new createNode(val)
   if (position >= 0 && position <= length) {
     let prev = head, 
         curr = head,
@@ -130,14 +162,13 @@ function insert (position, element) {
 // 测试
 list.insert(10)
 
-
 // 判断链表中是否存在某节点: 循环结束条件为 index++ < length
-function search(element) {
+function search(val) {
   if (!head) return false
   let p = head, index = 0
   // 和单链表的不同所在
   while(index++ < length) {
-    if (p.element === element) return true
+    if (p.val === val) return true
     p = p.next
   }
   return false
@@ -148,19 +179,19 @@ list.search(4) // true
 list.search(11) // false
 
 
-// 删除值为 element 节点: 循环结束条件为 index++ < length
-function remove (element) {
+// 删除值为 val 节点: 循环结束条件为 index++ < length
+function remove (val) {
   let p = head, prev = head, index = 0
   // 空链表
   if(!head) return
-  // 仅有一个节点且element一致
-  if(length === 1 && head.element === element){  
+  // 仅有一个节点且val一致
+  if(length === 1 && head.val === val){  
     head = null
     length-- 
     return  
   }
   while(index++ < length) {
-    if(p.element === element) {
+    if(p.val === val) {
       p = p.next
       prev.next = p
       length --
